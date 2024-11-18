34acc899a20118a062c390668a98b8e8c6d276b6
Brian DesArmo <31257901+briandesarmo@users.noreply.github.com>
2024-11-18 16:47:16 -0500

    Update Program.cs

--- src/WorkshopDemo/Program.cs
+++ src/WorkshopDemo/Program.cs
@@ -8,31 +8,71 @@ using WorkshopDemo.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.    
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddOpenApi();
-builder.Services.AddControllers();
-builder.Services.AddHealthChecks()
-    .AddCheck<WorkshopDemoHealthCheck>(nameof(WorkshopDemoHealthCheck));
-builder.Services.AddSingleton<IFileService, FileService>();
-builder.Services.AddSingleton<IVersionService, VersionService>();
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Configuration.AddAzureKeyVault(
-    new Uri($"https://kv-briandesarmo-{builder.Environment.EnvironmentName}.vault.azure.net/"),
-    new DefaultAzureCredential());
+     try
+     {
+         Log.Information("Starting web host");
 
-var app = builder.Build();
+         // Add services to the container.
+         builder.Host.UseSerilog((ctx, lc) => lc
+             .MinimumLevel.Information()
+             .MinimumLevel.Override("WorkshopDemo", LogEventLevel.Debug)
+             .MinimumLevel.Override("System", LogEventLevel.Error)
+             .WriteTo.Console(
+                 outputTemplate:
+                 "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}",
+                 theme: AnsiConsoleTheme.Literate)
+             .WriteTo.ApplicationInsights(
+                 new TelemetryConfiguration
+                     { ConnectionString = ctx.Configuration.GetConnectionString("ApplicationInsights") },
+                 TelemetryConverter.Traces)
+             .Enrich.FromLogContext());
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+         builder.Services.AddEndpointsApiExplorer();
+         builder.Services.AddOpenApi();
+         builder.Services.AddControllers();
+         builder.Services.AddHealthChecks()
+             .AddCheck<WorkshopDemoHealthCheck>(nameof(WorkshopDemoHealthCheck));
+         builder.Services.AddSingleton<IFileService, FileService>();
+         builder.Services.AddSingleton<IVersionService, VersionService>();
 
-app.UseHttpsRedirection();
+         builder.Configuration.AddAzureKeyVault(
+             new Uri($"https://kv-{YOURNAMEHERE}-{builder.Environment.EnvironmentName}.vault.azure.net/"),
+             new ChainedTokenCredential(new AzureCliCredential(), new ManagedIdentityCredential()));
 
-app.MapHealthChecks("/api/healthz");
+         builder.Services.AddApplicationInsightsTelemetry(options =>
+         {
+             options.ConnectionString = builder.Configuration.GetConnectionString("ApplicationInsights");
+         });
 
-app.MapControllers();
+         var app = builder.Build();
 
-app.Run();
+         app.UseSerilogRequestLogging();
+
+         // Configure the HTTP request pipeline.
+         if (app.Environment.IsDevelopment())
+         {
+             app.MapOpenApi();
+         }
+
+         app.UseHttpsRedirection();
+
+         app.MapHealthChecks("/api/healthz");
+
+         app.MapControllers();
+
+         app.Run();
+     }
+     catch (Exception ex)
+     {
+         Log.Fatal(ex, "Application terminated");
+     }
+     finally
+     {
+         Log.CloseAndFlush();
+     }
